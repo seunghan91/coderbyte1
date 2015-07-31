607275b177e84f28d9499fd7dc06e7d2a90c4cf4
seunghan Kim <theqwe2000@naver.com>
2015-08-01 00:38:27 +0900

    Create Alphabet Soup.rb

---
+++ Alphabet Soup.rb
@@ -0,0 +1,13 @@
+def AlphabetSoup(str)
+
+  # code goes here
+  str = str.split("").sort.join
+  return str 
+         
+end
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)  
+
+
+
+
